c8c5303cc537c5e71938c3ee61a19762903c3fa2
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-06-18 12:20:45 -0300

    feat: estrutura inicial do projeto

---
+++ automatizacao-testes-com-java/src/main/java/com/gabriel/ferreira/automatizacao_testes_com_java/AutomatizacaoTestesComJavaApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.ferreira.automatizacao_testes_com_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatizacaoTestesComJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomatizacaoTestesComJavaApplication.class, args);
+	}
+
+}
---
+++ automatizacao-testes-com-java/src/test/java/com/gabriel/ferreira/automatizacao_testes_com_java/AutomatizacaoTestesComJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.ferreira.automatizacao_testes_com_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomatizacaoTestesComJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
