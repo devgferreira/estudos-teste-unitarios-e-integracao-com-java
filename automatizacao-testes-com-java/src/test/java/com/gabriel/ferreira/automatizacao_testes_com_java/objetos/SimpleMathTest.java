17e47a6e11db4247148d6fc370858240df1f858d
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-06-18 12:32:13 -0300

    feat: criando a classe teste para SimpleMath - SimpleMathTest

---
+++ automatizacao-testes-com-java/src/test/java/com/gabriel/ferreira/automatizacao_testes_com_java/objetos/SimpleMathTest.java
@@ -0,0 +1,10 @@
+package com.gabriel.ferreira.automatizacao_testes_com_java.objetos;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimpleMathTest {
+
+
+}
