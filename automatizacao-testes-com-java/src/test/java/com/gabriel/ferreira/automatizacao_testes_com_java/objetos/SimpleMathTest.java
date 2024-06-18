72702145ff705d795e0e4196195e7a52817066e5
Gabriel Ferreira Souto <gabrielferreirasouto.com@gmail.com>
2024-06-18 12:52:23 -0300

    feat: criando o teste: testSum_comValoresValidos_RetornadoSum - SimpleMathTest

--- automatizacao-testes-com-java/src/test/java/com/gabriel/ferreira/automatizacao_testes_com_java/objetos/SimpleMathTest.java
+++ automatizacao-testes-com-java/src/test/java/com/gabriel/ferreira/automatizacao_testes_com_java/objetos/SimpleMathTest.java
@@ -4,7 +4,34 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
 class SimpleMathTest {
 
+    @Test
+    void testSum_comValoresValidos_RetornadoSum() {
+        SimpleMath math = new SimpleMath();
+        Double firstNumber = 6.2D;
+        Double secondNumber = 2D;
+        Double result = math.sum(firstNumber, secondNumber);
+        Double espera = 8.2D;
+        assertEquals(result, espera, () ->
+                "testSum_comValoresValidos_RetornadoSum(), não fundionou da forma esperada. " + firstNumber + "+"
+                        + secondNumber + " espera: "+ espera);
+    }
 
+    @Test
+    void sub() {
+    }
+
+    @Test
+    void mult() {
+    }
+
+    @Test
+    void div() {
+    }
+
+    @Test
+    void mean() {
+    }
 }
