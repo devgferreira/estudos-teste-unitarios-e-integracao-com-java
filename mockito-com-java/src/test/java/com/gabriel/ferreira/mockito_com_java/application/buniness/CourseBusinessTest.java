0090975a219b45b3f954787c72b9a185f2864456
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2024-06-19 11:58:22 -0300

    test: criando a classe de teste: CourseBusinessTest

---
+++ mockito-com-java/src/test/java/com/gabriel/ferreira/mockito_com_java/application/buniness/CourseBusinessTest.java
@@ -0,0 +1,14 @@
+package com.gabriel.ferreira.mockito_com_java.application.buniness;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CourseBusinessTest {
+    @Test
+    void testbuscarCursosPorEstudante_UsandoStub(){
+        //Given / Arrange
+    }
+
+
+}
