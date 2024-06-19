5f2d1c3d5882a0c911db7f5800b7d092c86766be
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2024-06-19 10:28:41 -0300

    feat: feat: criando a service: PersonService

---
+++ tdd-com-java-junit/src/main/java/com/gabriel/ferreira/tdd_com_java/application/service/PersonService.java
@@ -0,0 +1,11 @@
+package com.gabriel.ferreira.tdd_com_java.application.service;
+
+import com.gabriel.ferreira.tdd_com_java.application.interfaces.IPersonService;
+
+public class PersonService implements IPersonService {
+
+    @Override
+    public void criarPerson() {
+
+    }
+}
