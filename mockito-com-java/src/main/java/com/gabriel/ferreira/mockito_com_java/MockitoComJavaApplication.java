6123d02eb607d69dc448bb994c216e8ef23567cb
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2024-06-19 11:38:22 -0300

    feat: estrutura inicial do projeto

---
+++ mockito-com-java/src/main/java/com/gabriel/ferreira/mockito_com_java/MockitoComJavaApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.ferreira.mockito_com_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockitoComJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockitoComJavaApplication.class, args);
+	}
+
+}
---
+++ mockito-com-java/src/test/java/com/gabriel/ferreira/mockito_com_java/MockitoComJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.ferreira.mockito_com_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockitoComJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
