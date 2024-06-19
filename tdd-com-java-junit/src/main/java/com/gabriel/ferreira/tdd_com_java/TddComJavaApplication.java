41dc3e62df0ca9ad3de8574d72e2f1c7ead929db
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2024-06-19 09:54:26 -0300

    feat: subindo a estrutura inicial do projeto

---
+++ tdd-com-java-junit/src/main/java/com/gabriel/ferreira/tdd_com_java/TddComJavaApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.ferreira.tdd_com_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddComJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddComJavaApplication.class, args);
+	}
+
+}
---
+++ tdd-com-java-junit/src/test/java/com/gabriel/ferreira/tdd_com_java/TddComJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.ferreira.tdd_com_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddComJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
