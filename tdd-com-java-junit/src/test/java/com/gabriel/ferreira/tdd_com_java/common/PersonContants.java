cfb73a6fe4b746348b755dd7e781c27493722a0f
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2024-06-19 10:48:19 -0300

    feat: criando a constant: PERSON_VALIDA - PersonContants

---
+++ tdd-com-java-junit/src/test/java/com/gabriel/ferreira/tdd_com_java/common/PersonContants.java
@@ -0,0 +1,13 @@
+package com.gabriel.ferreira.tdd_com_java.common;
+
+import com.gabriel.ferreira.tdd_com_java.domain.model.Person;
+
+public class PersonContants {
+    public static final Person PERSON_VALIDA = new Person(
+                "Gabriel",
+                        "Moon",
+                        "gabi@gmail.com",
+                        "Rua - SP",
+                        "M"
+    );
+}
