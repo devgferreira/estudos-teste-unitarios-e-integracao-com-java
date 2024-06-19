8e118ce5dfab38d34cff6dbdaca14fca33038fba
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2024-06-19 12:02:27 -0300

    test: criando o teste: testbuscarCursosPorEstudante_UsandoStub - CourseBusinessTest

--- mockito-com-java/src/test/java/com/gabriel/ferreira/mockito_com_java/application/buniness/CourseBusinessTest.java
+++ mockito-com-java/src/test/java/com/gabriel/ferreira/mockito_com_java/application/buniness/CourseBusinessTest.java
@@ -1,6 +1,11 @@
 package com.gabriel.ferreira.mockito_com_java.application.buniness;
 
+import com.gabriel.ferreira.mockito_com_java.application.interfaces.ICourseService;
+import com.gabriel.ferreira.mockito_com_java.application.services.CourseService;
 import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,6 +13,14 @@ class CourseBusinessTest {
     @Test
     void testbuscarCursosPorEstudante_UsandoStub(){
         //Given / Arrange
+        ICourseService _courseService = new CourseService();
+        CourseBusiness _courseBusiness = new CourseBusiness(_courseService);
+
+        //When / Act
+        List<String> cursosFiltrados = _courseBusiness.buscarCursosPorEstudante("Gabriel");
+
+        // Then / Assert
+        assertEquals(4, cursosFiltrados.size());
     }
 
 
