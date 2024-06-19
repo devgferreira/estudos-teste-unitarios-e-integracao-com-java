6eda8ad3f2f419c8925e1748bf868c5d34808a4e
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2024-06-19 10:38:13 -0300

    feat: refatorando o método criarPerson para retorna uma person - PersonService.java

--- tdd-com-java-junit/src/main/java/com/gabriel/ferreira/tdd_com_java/application/service/PersonService.java
+++ tdd-com-java-junit/src/main/java/com/gabriel/ferreira/tdd_com_java/application/service/PersonService.java
@@ -1,11 +1,13 @@
 package com.gabriel.ferreira.tdd_com_java.application.service;
 
 import com.gabriel.ferreira.tdd_com_java.application.interfaces.IPersonService;
+import com.gabriel.ferreira.tdd_com_java.domain.model.Person;
 
 public class PersonService implements IPersonService {
 
     @Override
-    public void criarPerson() {
+    public Person criarPerson(Person person) {
 
+        return person;
     }
 }
