5b654db4470cc5df9f360f7f3a3048386b0cfe47
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2024-06-19 10:44:39 -0300

    test: criando o teste: testCriarPerson_ComSucessoSeConterPimerioNome_RetornandoPerson - PersonServiceTest

--- tdd-com-java-junit/src/test/java/com/gabriel/ferreira/tdd_com_java/application/service/PersonServiceTest.java
+++ tdd-com-java-junit/src/test/java/com/gabriel/ferreira/tdd_com_java/application/service/PersonServiceTest.java
@@ -2,10 +2,10 @@ package com.gabriel.ferreira.tdd_com_java.application.service;
 
 import com.gabriel.ferreira.tdd_com_java.application.interfaces.IPersonService;
 import com.gabriel.ferreira.tdd_com_java.domain.model.Person;
+import lombok.Data;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 class PersonServiceTest {
     @Test
     void testCriarPerson_ComPersonValida_RetornandoPerson(){
@@ -24,6 +24,24 @@ class PersonServiceTest {
 
         //Then / Assert
         assertEquals(result, person, () -> "O criarPerson(), não retornou uma Person.");
+    }
+    @Test
+    void testCriarPerson_ComSucessoSeConterPimerioNome_RetornandoPerson(){
+        //Given / Arrange
+        IPersonService _personService = new PersonService();
+        Person person = new Person(
+                "Gabriel",
+                "Moon",
+                "gabi@gmail.com",
+                "Rua - SP",
+                "M"
+        );
+
+        // When / Act
+        Person result = _personService.criarPerson(person);
+
+        //Then / Assert
+        assertEquals(result.getPrimeiroNome(), person.getPrimeiroNome(), () -> "O primerio nome é diferente.");
     }
 
 }
