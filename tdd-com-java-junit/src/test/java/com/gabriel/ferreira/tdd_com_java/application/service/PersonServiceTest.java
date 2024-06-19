7adaada6d692ccbde53b70db53f296fd93a0a576
Gabriel Ferreira <gabrielferreirasouto.com@gmail.com>
2024-06-19 10:53:50 -0300

    test: criando o teste: testCriarPerson_ComSucessoSeTodosOsAtributosForemValidos_RetornandoPerson - PersonServiceTest

--- tdd-com-java-junit/src/test/java/com/gabriel/ferreira/tdd_com_java/application/service/PersonServiceTest.java
+++ tdd-com-java-junit/src/test/java/com/gabriel/ferreira/tdd_com_java/application/service/PersonServiceTest.java
@@ -31,5 +31,21 @@ class PersonServiceTest {
         //Then / Assert
         assertEquals(result.getPrimeiroNome(), PERSON_VALIDA.getPrimeiroNome(), () -> "O primerio nome é diferente.");
     }
+    @Test
+    void testCriarPerson_ComSucessoSeTodosOsAtributosForemValidos_RetornandoPerson(){
+        //Given / Arrange
+        IPersonService _personService = new PersonService();
+
+
+        // When / Act
+        Person result = _personService.criarPerson(PERSON_VALIDA);
+
+        //Then / Assert
+        assertEquals(result.getPrimeiroNome(), PERSON_VALIDA.getPrimeiroNome(), () -> "O primerio nome é diferente.");
+        assertEquals(result.getSegundoNome(), PERSON_VALIDA.getSegundoNome(), () -> "O primerio segundo é diferente.");
+        assertEquals(result.getEndereco(), PERSON_VALIDA.getEndereco(), () -> "O endereço é diferente.");
+        assertEquals(result.getEmail(), PERSON_VALIDA.getEmail(), () -> "O email é diferente.");
+        assertEquals(result.getGenero(), PERSON_VALIDA.getGenero(), () -> "O genero é diferente.");
+    }
 
 }
